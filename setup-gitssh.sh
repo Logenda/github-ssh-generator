b27dd729b74f0f0f339803b8a0176a57afbc7f2e
Plads <34081873+PladsElsker@users.noreply.github.com>
2024-08-29 22:53:33 -0400

    Update setup-gitssh.sh

--- setup-gitssh.sh
+++ setup-gitssh.sh
@@ -28,3 +28,5 @@ ssh-keygen -t ed25519 -C "$email"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 cat ~/.ssh/id_ed25519.pub
+git config --global user.email $email
+git config --global pull.rebase false
